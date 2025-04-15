f41819a8e89077b6a2b9d2053f576da57ab3f24a
Hokage-xMinato <minatonamikaze4756@gmail.com>
2025-04-16 00:12:17 +0530

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 const classLinks = {
     firstClassLink: "", // Replace with your first class link
-    secondClassLink: "" // Replace with your second class link
+    secondClassLink: "https://www.youtube.com/live/EVU_Bvc3HpE?si=ZLV5YL1ZtSNTwMNb" // Replace with your second class link
 };
 
 
